fcc2789cbb3407cc1abab194a010ec12b8dbcd95
johnyguido <johnyguido@hotmail.com.br>
2022-09-19 19:44:57 -0300

    Adjust Project HREureka server

--- hr-eureka-server/src/test/java/com/johny/eureka/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/johny/eureka/HrEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.johny.hreurekaserver;
+package com.johny.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HrEurekaServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
